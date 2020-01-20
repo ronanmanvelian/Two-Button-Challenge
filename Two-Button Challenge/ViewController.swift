aba9a0febd00350a1a7467c797c6ffe076dc897f
Ronan Manvelian <manvelia@bc.edu>
2020-01-20 13:41:52 -0500

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Ronan Manvelian on 1/20/20.
+//  Copyright © 2020 Ronan Manvelian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
